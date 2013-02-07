5084cb30cb0c238701d7d2924e0ceb4bb6614eff
Reiner Jung <rju@informatik.uni-kiel.de>
2013-02-07 17:58:17 +0100

    Removed unused imports

--- de.cau.cs.se.kieker.service.eclipse/src/de/cau/cs/se/kieker/service/eclipse/launch/ServiceSetupTab.java
+++ de.cau.cs.se.kieker.service.eclipse/src/de/cau/cs/se/kieker/service/eclipse/launch/ServiceSetupTab.java
@@ -45,7 +45,6 @@ import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Group;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Text;
-import org.eclipse.swt.widgets.Widget;
 import org.eclipse.ui.dialogs.ElementListSelectionDialog;
 import org.eclipse.ui.dialogs.FilteredResourcesSelectionDialog;
 import org.eclipse.ui.model.WorkbenchLabelProvider;
