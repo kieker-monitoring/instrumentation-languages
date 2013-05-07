b1f2270d28f76ceb3a20457ba195e3b8ab84e1c9
Reiner Jung <rju@informatik.uni-kiel.de>
2013-05-07 11:09:11 +0200

    Updated instrumentation languages and added tyco/maven build system to the projects

---
+++ de.cau.cs.se.instrumentation.al.tests/src/de/cau/cs/se/instrumentation/al/tests/DummyTest.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package de.cau.cs.se.instrumentation.al.tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * @author rju
+ *
+ */
+public class DummyTest {
+
+	@Test
+	public void test() {
+		 assertTrue(true);
+	}
+
+}
