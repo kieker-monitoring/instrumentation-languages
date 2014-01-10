9c9e45ffc4e32eec6b17eec303700031f4418aa3
Reiner Jung <rju@informatik.uni-kiel.de>
2014-01-10 16:36:12 +0100

    added new files

---
+++ de.cau.cs.se.instrumentation.rl/xtend-gen/de/cau/cs/se/instrumentation/rl/generator/InternalErrorException.java
@@ -0,0 +1,8 @@
+package de.cau.cs.se.instrumentation.rl.generator;
+
+@SuppressWarnings("all")
+public class InternalErrorException extends Exception {
+  public InternalErrorException(final String message) {
+    super(message);
+  }
+}
