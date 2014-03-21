f7de024ec1f3449c1d0b96b04da899a46b7a8325
Reiner Jung <rju@informatik.uni-kiel.de>
2014-03-21 15:15:33 +0100

    still too stupid to set the path correctly :-(

--- provide-results.sh
+++ provide-results.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 #
 # Create archive for IRL compiler
+export COMPILER_DIR="$WORKSPACE/de.cau.cs.se.instrumentationrl.cli"
 
 # irl compiler
-tar -cvzpf $WORKSPACE/irl-compiler.tar.gz $WORKSPACE/run-irl-compiler.sh $WORKSPACE/lib $WORKSPACE/target/de.cau.cs.se.instrumentation.rl.cli-*.jar
+tar -cvzpf $WORKSPACE/irl-compiler.tar.gz $COMPILER_DIR/run-irl-compiler.sh $COMPILER_DIR/lib $COMPILER_DIR/target/de.cau.cs.se.instrumentation.rl.cli-*.jar
 
 # end
