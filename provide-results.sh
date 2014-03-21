3d9dabcf63a4f777b592b912a8f5ee9292e8669f
Reiner Jung <rju@informatik.uni-kiel.de>
2014-03-21 15:17:57 +0100

    again

--- provide-results.sh
+++ provide-results.sh
@@ -4,6 +4,7 @@
 export COMPILER_DIR="$WORKSPACE/de.cau.cs.se.instrumentationrl.cli"
 
 # irl compiler
-tar -cvzpf $WORKSPACE/irl-compiler.tar.gz $COMPILER_DIR/run-irl-compiler.sh $COMPILER_DIR/lib $COMPILER_DIR/target/de.cau.cs.se.instrumentation.rl.cli-*.jar
+cd $COMPILER_DIR
+tar -cvzpf $WORKSPACE/irl-compiler.tar.gz run-irl-compiler.sh lib target/de.cau.cs.se.instrumentation.rl.cli-*.jar
 
 # end
