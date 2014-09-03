ee946899cc9a953ae6fb21511100f4f27527b5d6
Reiner Jung <rju@informatik.uni-kiel.de>
2014-09-03 12:17:52 +0200

    added missing generated files

---
+++ de.cau.cs.se.instrumentation.rl/src-gen/de/cau/cs/se/instrumentation/rl/recordLang/TemplateType.java
@@ -0,0 +1,18 @@
+/**
+ */
+package de.cau.cs.se.instrumentation.rl.recordLang;
+
+
+/**
+ * <!-- begin-user-doc -->
+ * A representation of the model object '<em><b>Template Type</b></em>'.
+ * <!-- end-user-doc -->
+ *
+ *
+ * @see de.cau.cs.se.instrumentation.rl.recordLang.RecordLangPackage#getTemplateType()
+ * @model
+ * @generated
+ */
+public interface TemplateType extends Type
+{
+} // TemplateType
---
+++ de.cau.cs.se.instrumentation.rl/src-gen/de/cau/cs/se/instrumentation/rl/recordLang/impl/TemplateTypeImpl.java
@@ -0,0 +1,42 @@
+/**
+ */
+package de.cau.cs.se.instrumentation.rl.recordLang.impl;
+
+import de.cau.cs.se.instrumentation.rl.recordLang.RecordLangPackage;
+import de.cau.cs.se.instrumentation.rl.recordLang.TemplateType;
+
+import org.eclipse.emf.ecore.EClass;
+
+/**
+ * <!-- begin-user-doc -->
+ * An implementation of the model object '<em><b>Template Type</b></em>'.
+ * <!-- end-user-doc -->
+ * <p>
+ * </p>
+ *
+ * @generated
+ */
+public class TemplateTypeImpl extends TypeImpl implements TemplateType
+{
+  /**
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   */
+  protected TemplateTypeImpl()
+  {
+    super();
+  }
+
+  /**
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   */
+  @Override
+  protected EClass eStaticClass()
+  {
+    return RecordLangPackage.Literals.TEMPLATE_TYPE;
+  }
+
+} //TemplateTypeImpl
