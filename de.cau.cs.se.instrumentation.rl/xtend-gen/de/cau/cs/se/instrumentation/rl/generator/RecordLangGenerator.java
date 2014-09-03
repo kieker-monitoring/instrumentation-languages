3b541604a29fb61114535526fdcf350afcb02079
Reiner Jung <rju@informatik.uni-kiel.de>
2014-09-03 10:46:51 +0200

    minor modifications

--- de.cau.cs.se.instrumentation.rl/xtend-gen/de/cau/cs/se/instrumentation/rl/generator/RecordLangGenerator.java
+++ de.cau.cs.se.instrumentation.rl/xtend-gen/de/cau/cs/se/instrumentation/rl/generator/RecordLangGenerator.java
@@ -49,7 +49,7 @@ public class RecordLangGenerator implements IGenerator {
         final String author = TargetsPreferences.getAuthorName();
         for (final Class<?> generator : GeneratorConfiguration.recordTypeGenerators) {
           {
-            Constructor<?> _constructor = generator.getConstructor();
+            Constructor<? extends Object> _constructor = generator.getConstructor();
             Object _newInstance = _constructor.newInstance();
             final AbstractRecordTypeGenerator cg = ((AbstractRecordTypeGenerator) _newInstance);
             String _id = cg.getId();
@@ -74,7 +74,7 @@ public class RecordLangGenerator implements IGenerator {
         }
         for (final Class<?> generator_1 : GeneratorConfiguration.partialRecordTypeGenerators) {
           {
-            Constructor<?> _constructor = generator_1.getConstructor();
+            Constructor<? extends Object> _constructor = generator_1.getConstructor();
             Object _newInstance = _constructor.newInstance();
             final AbstractPartialRecordTypeGenerator cg = ((AbstractPartialRecordTypeGenerator) _newInstance);
             String _id = cg.getId();
