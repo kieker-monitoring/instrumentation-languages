c2745c649f41e4442f7b8a7e24ef354d83c2690b
Reiner Jung <rju@informatik.uni-kiel.de>
2014-11-25 17:06:26 +0100

    update snapshot version values in files

--- de.cau.cs.se.instrumentation.releng/scripts/set-release-properties.sh
+++ de.cau.cs.se.instrumentation.releng/scripts/set-release-properties.sh
@@ -1,18 +1,14 @@
 #!/bin/bash
 
-echo "this script is broken and should not be used."
-exit 1
-
 # This script must check the following files:
 # all pom.xml files but only change the package version tags value and not of any other element of the file
 # fix the feature.xml files in the SDK projects
 # fix the MANIFEST.MF files
 # fix the category.xml file
 # fix in cli pom the reference to the rl package version
-# please make sue that all these requirements are fulfilled before automating things again
+# please make sure that all these requirements are fulfilled before automating things again
 
 BINDIR=$(cd "$(dirname "$0")"; pwd)/
-UPDATESITE="${BINDIR}/../../de.cau.cs.se.instrumentation.updatesite"
 
 if [ "$1" == "" ] ; then
 	echo "Usage: $0 <version> <repo-rel-path>"
@@ -25,16 +21,37 @@ if [ "$2" == "" ] ; then
 	echo "Usage: $0 <version> <repo-rel-path>"
 	exit 1
 else
-	REL_PATH=`echo "$2" | sed 's/\//\\\//g'`
+	REL_PATH=`echo "$2" | sed 's/\//\\\\\//g'`
 fi
 
 TMP=`tempfile`
 
-# fix pom.xml files
-for I in `find ${BINDIR}/../.. -name 'pom.xml'` ; do
-	echo $I
-	cat "$I" | sed "s/\(<version>\).*\(<\/version>\)/\1${VERSION}\2/g" > "$TMP"
-	cp "$TMP" "$I"
+# create pom.xml files
+for I in `find ${BINDIR}/../.. -name 'pom.xml.template'` ; do
+	J=`echo "$I" | sed 's/\.template$//g'`
+	echo $J
+	cat "$I" | sed "s/#VERSION#/${VERSION}/g" | sed "s/#UPDATESITE#/${REL_PATH}/g" > "$J"
+done
+
+# create feature files
+for I in `find ${BINDIR}/../.. -name 'feature.xml.template'` ; do
+	J=`echo "$I" | sed 's/\.template$//g'`
+	echo $J
+	cat "$I" | sed "s/#VERSION#/${VERSION}/g" | sed "s/#UPDATESITE#/${REL_PATH}/g" > "$J"
+done
+
+# create feature files
+for I in `find ${BINDIR}/../.. -name 'feature.xml.template'` ; do
+	J=`echo "$I" | sed 's/\.template$//g'`
+	echo $J
+	cat "$I" | sed "s/#VERSION#/${VERSION}/g" | sed "s/#UPDATESITE#/${REL_PATH}/g" > "$J"
+done
+
+# create category files
+for I in `find ${BINDIR}/../.. -name 'category.xml.template'` ; do
+	J=`echo "$I" | sed 's/\.template$//g'`
+	echo $J
+	cat "$I" | sed "s/#VERSION#/${VERSION}/g" | sed "s/#UPDATESITE#/${REL_PATH}/g" > "$J"
 done
 
 # fix manifest files
@@ -44,9 +61,7 @@ for I in `find ${BINDIR}/../.. -name 'MANIFEST.MF'` ; do
 	cp "$TMP" "$I"
 done
 
-# fix update site path
-cat "${UPDATESITE}/pom.xml" | sed "s/\(<updatesite.path>file:\/\/\/srv\/vhosts\/default\/htdocs\/eus\/\).*\(<\/updatesite.path>\)/\1${REL_PATH}\2/g" > "$TMP"
-cp "$TMP" "${UPDATESITE}/pom.xml"
+# fix category xml
 
 # end
 
