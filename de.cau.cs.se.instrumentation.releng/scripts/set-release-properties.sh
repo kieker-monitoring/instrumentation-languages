c5e6a972aa13351219af546fed59f94a10088242
Reiner Jung <rju@informatik.uni-kiel.de>
2014-11-25 11:20:04 +0100

    moved update site

---
+++ de.cau.cs.se.instrumentation.releng/scripts/set-release-properties.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+BINDIR=$(cd "$(dirname "$0")"; pwd)/
+UPDATESITE="${BINDIR}/../../de.cau.cs.sr.instrumentation.updatesite"
+
+if [ "$1" == "" ] ; then
+	echo "Usage: $0 <version> <repo-rel-path>"
+	exit 1
+else
+	VERSION="$1"
+fi
+
+if [ "$2" == "" ] ; then
+	echo "Usage: $0 <version> <repo-rel-path>"
+	exit 1
+else
+	REL_PATH="$2"
+fi
+
+TMP=`tempfile`
+
+# fix pom.xml files
+for I in `find ${BINDIR}/../.. -name 'pom.xml'` ; do
+	echo $I
+	cat "$I" | sed "s/\(<version>\).*\(<\/version>\)/\1${VERSION}\2/g" > "$TMP"
+	cp "$TMP" "$I"
+done
+
+# fix manifest files
+for I in `find ${BINDIR}/../.. -name 'MANIFEST.MF'` ; do
+	echo $I
+	cat "$I" |  sed "s/\(Bundle-Version: \).*/\1${VERSION}/g" > "$TMP"
+	cp "TMP" "$I"
+done
+
+# fix update site path
+cat "${UPDATESITE}/pom.xml" | sed "s/\(<updatesite.path>file:\/\/\/srv\/vhosts\/default\/htdocs\/eus\/\).*\(<\/updatesite.path>\)/\1${REL_PATH}\2/g" > "$TMP"
+cp "$TMP" "${UPDATESITE}/pom.xml"
+
+# end
+
