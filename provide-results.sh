a3ec97ce0fc209274a316be50b0bda9bf56f9d22
Reiner Jung <reiner.jung@email.uni-kiel.de>
2015-03-22 23:05:26 +0100

    fixed tar command, removed lib dir from parameters in result generation script.

--- provide-results.sh
+++ provide-results.sh
@@ -5,6 +5,6 @@ export COMPILER_DIR="$WORKSPACE/de.cau.cs.se.instrumentation.rl.cli"
 
 # irl compiler
 cd $COMPILER_DIR
-tar -cvzpf $WORKSPACE/irl-compiler.tar.gz run-irl-compiler.sh lib target/de.cau.cs.se.instrumentation.rl.cli-*.jar
+tar -cvzpf $WORKSPACE/irl-compiler.tar.gz run-irl-compiler.sh target/de.cau.cs.se.instrumentation.rl.cli-*.jar
 
 # end
