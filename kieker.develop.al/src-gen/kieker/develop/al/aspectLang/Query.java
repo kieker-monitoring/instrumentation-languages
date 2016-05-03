e10267629c1f866dd667c5902e5e48d5a820559e
Reiner Jung <reiner.jung@email.uni-kiel.de>
2016-05-03 17:41:03 +0200

    added missing files

---
+++ kieker.develop.al/src-gen/kieker/develop/al/aspectLang/Query.java
@@ -0,0 +1,19 @@
+/**
+ */
+package kieker.develop.al.aspectLang;
+
+import org.eclipse.emf.ecore.EObject;
+
+/**
+ * <!-- begin-user-doc -->
+ * A representation of the model object '<em><b>Query</b></em>'.
+ * <!-- end-user-doc -->
+ *
+ *
+ * @see kieker.develop.al.aspectLang.AspectLangPackage#getQuery()
+ * @model
+ * @generated
+ */
+public interface Query extends EObject
+{
+} // Query
---
+++ kieker.develop.al/src-gen/kieker/develop/al/aspectLang/impl/QueryImpl.java
@@ -0,0 +1,42 @@
+/**
+ */
+package kieker.develop.al.aspectLang.impl;
+
+import kieker.develop.al.aspectLang.AspectLangPackage;
+import kieker.develop.al.aspectLang.Query;
+
+import org.eclipse.emf.ecore.EClass;
+
+import org.eclipse.emf.ecore.impl.MinimalEObjectImpl;
+
+/**
+ * <!-- begin-user-doc -->
+ * An implementation of the model object '<em><b>Query</b></em>'.
+ * <!-- end-user-doc -->
+ *
+ * @generated
+ */
+public class QueryImpl extends MinimalEObjectImpl.Container implements Query
+{
+  /**
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   */
+  protected QueryImpl()
+  {
+    super();
+  }
+
+  /**
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   */
+  @Override
+  protected EClass eStaticClass()
+  {
+    return AspectLangPackage.Literals.QUERY;
+  }
+
+} //QueryImpl
