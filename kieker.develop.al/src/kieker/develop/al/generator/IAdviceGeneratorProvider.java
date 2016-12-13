3ad4c9af0fe70eabfd4575c5746e9c3a37b9dde8
Reiner Jung <reiner.jung@email.uni-kiel.de>
2016-12-13 17:48:05 +0100

    adding comments cleanups

--- kieker.develop.al/src/kieker/develop/al/generator/IAdviceGeneratorProvider.java
+++ kieker.develop.al/src/kieker/develop/al/generator/IAdviceGeneratorProvider.java
@@ -1,8 +1,35 @@
+/***************************************************************************
+ * Copyright 2016 Kieker Project (http://kieker-monitoring.net)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ***************************************************************************/
 package kieker.develop.al.generator;
 
 import java.util.Collection;
 
+/**
+ * Outlet configuration provider registration interface for advice generation.
+ *
+ * @author Reiner Jung
+ *
+ */
 public interface IAdviceGeneratorProvider {
 
-    public void addOutletConfigurations(Collection<AbstractOutletConfiguration> configurations);
+	/**
+	 * Add outlet configurations to the collection of outlet configurations.
+	 *
+	 * @param configurations
+	 *            the collection of configurations
+	 */
+	public void addOutletConfigurations(Collection<AbstractOutletConfiguration> configurations);
 }
