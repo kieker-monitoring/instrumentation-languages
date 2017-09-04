d557c0a3b630491db8a98879c1f873f8555b19cf
Reiner Jung <reiner.jung@email.uni-kiel.de>
2017-09-04 18:40:50 +0200

    improved readability of test.

--- kieker.develop.rl/test/kieker/develop/rl/generator/TestAbstractTypeGenerator.java
+++ kieker.develop.rl/test/kieker/develop/rl/generator/TestAbstractTypeGenerator.java
@@ -21,7 +21,7 @@ public class TestAbstractTypeGenerator {
 	private static final String HEADER = "HEADER";
 	private static final String AUTHOR = "AUTHOR";
 	private static final String VERSION = "VERSION";
-	private static final Version TARGET_VERSION = new Version("1.2");
+	private static final String TARGET_VERSION = "1.2";
 	private AbstractTypeGenerator<ComplexType, String> generator;
 	private final ComplexType type = RecordLangFactory.eINSTANCE.createEventType();
 
@@ -46,7 +46,7 @@ public class TestAbstractTypeGenerator {
 			
 		};
 		type.setName("EmptyRecord");
-		generator.configure("1.2", HEADER, AUTHOR, VERSION);
+		generator.configure(TARGET_VERSION, HEADER, AUTHOR, VERSION);
 	}
 
 	/**
@@ -54,13 +54,24 @@ public class TestAbstractTypeGenerator {
 	 */
 	@Test
 	public void testIsSupported() {
-		assertEquals("Range error, 1.2 should be in 1.0:1.2 ", true, generator.isSupported("1.0:1.2"));
-		assertEquals("Range error, 1.2 should be in 1.2:0:1.3 ", true, generator.isSupported("1.2:1.3"));
-		assertEquals("Range error, 1.2 should be out 1:2.1:1.3 ", false, generator.isSupported("1.2.1:1.3"));
-		assertEquals("Range error, 1.2 should be in 1.0:", true, generator.isSupported("1.0:"));
-		assertEquals("Range error, 1.2 should be in :1.2 ", true, generator.isSupported(":1.2"));
-		assertEquals("Range error, 1.2 should be in 1.2:", true, generator.isSupported("1.2:"));
-		assertEquals("Range error, 1.2 should be out 1.2.1:", true, generator.isSupported("1.2.1:"));
+		String[] inRanges = new String[] {
+				"1.0:1.2",
+				"1.2:1.3",
+				"1.0:",
+				":1.2",
+				"1.2:",
+				"1.2:1.3",				
+		};
+		for (String range : inRanges) {
+			assertEquals("Range error, " + TARGET_VERSION + " should be in " + range, true, generator.isSupported(range));
+		}
+		String[] outRanges = new String[] {
+				"1.2.1:1.3",
+				"1.2.1:",
+		};
+		for (String range : outRanges) {
+			assertEquals("Range error, " + TARGET_VERSION + " should be out " + range, true, generator.isSupported(range));
+		}
 	}
 	
 	/**
@@ -68,7 +79,7 @@ public class TestAbstractTypeGenerator {
 	 */
 	@Test
 	public void testCreateOutputModel() {
-		String[] result = generator.createOutputModel(type, TARGET_VERSION, HEADER, AUTHOR, VERSION).split("::");
+		String[] result = generator.createOutputModel(type, new Version(TARGET_VERSION), HEADER, AUTHOR, VERSION).split("::");
 		assertEquals("Missing parameter", 4, result.length);
 		assertArrayEquals("Wrong values in code generation", new String[] { type.getName(), HEADER, AUTHOR, VERSION }, result);
 	}
